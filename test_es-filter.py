0f269134d065267b13f7699c924d46633b4cebbc
Marie <37214302+MarieT1D@users.noreply.github.com>
2018-09-19 14:02:49 +0200

    Update test_es-filter.py

--- test_es-filter.py
+++ test_es-filter.py
@@ -11,7 +11,6 @@ x=np.zeros_like(werte)
 for i in range(len(x)):
     x[i]=i*5
 print(werte,x)
-plt.plot(x,werte, linestyle='none', marker='.', label='origin')
 
 
 #forecasting
@@ -20,9 +19,18 @@ fc=[werte[0]]
 for i in range(len(werte)-1):
     a=int(fc[i]+p*(werte[i]-fc[i]))
     fc.append(a)
+    
+#15 min average
+av=[werte[0],werte[1]]
+for i in range(len(werte)-2):
+    a=int((werte[i]+werte[i+1]+werte[i+2])/3)
+    av.append(a)
+ 
   
 
 
+plt.plot(x,werte, linestyle='none', marker='.', label='origin')
 plt.plot(x,fc, linestyle='none', marker='.', label='filtered, p=' + str(p))
+plt.plot(x,av, linestyle='none', marker='.', label='15 min delta')
 plt.legend()
 
